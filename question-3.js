9a286609ec1b4976ac16a945f6a80d93fea84d6d
iMookatayou <jetsribumrungtechin@gmail.com>
2026-08-10 11:01:10 +0700

    feat(answer): algorithm skill checkpoint

--- question-3.js
+++ question-3.js
@@ -1,20 +1,20 @@
 /*
 What is the Big O notation for Function findStudentById . Explain your reason.
-Answer:
+Answer: O(n) เพราะใช้ Linear Search ซึ่งต้องตรวจสอบข้อมูลทีละตัว และในกรณีที่ข้อมูลอยู่ตัวสุดท้ายหรือไม่พบข้อมูล อาจต้องตรวจสอบนักเรียนทุกคนใน Array
 
 
 */
 
 /* 
 What is the Big O notation for Function findProductPrice. Explain your reason.
-Answer:
+Answer: O(log n) เพราะใช้ Binary Search ข้อมูลสินค้าถูกเรียงรายชื่ออยู่แล้ว โดยแต่ละรอบจะตัดขอบเขตการค้นหาออกไปครึ่งนึง
 
 
 */
 
-/* 
+/*
 Which function is more efficient and why?
-Answer:
+Answer: findProductPrice, Binary O(log n) เนื่องจากว่าจำนวนรอบในการ Loop น้อยกว่าทำให้ลดขอบเขตลงครึ่งนึงในแต่ละรอบ แต่ต้องใช้กับข้อมูลที่ถูกเรียงลำดับแล้ว
 
 
 */
--- question-5.js
+++ question-5.js
@@ -1,6 +1,6 @@
 /*  
 What is the Big O notation for Function sortProductsByPrice of the Question 4. Explain your reason.
-Answer:
+Answer: O(n2) ซ้อนกัน 2 ชั้น โดย Loop ด้านนอกทำงานประมาณ n รอบ และในแต่ละรอบ Loop ด้านในก็ทำงานประมาณ n รอบ จึงมีการทำงานประมาณ n × n = n2 ครั้ง
 
 
 */
